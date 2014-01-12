408a9dc9f8a8e11cddcf23395b9c3adfe4a4de47
Sami Koskinen <sami.koskinen@helsinki.fi>
2014-01-12 22:26:16 +0200

    rails app structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ratebeer::Application.config.secret_token = '230924a168ae8b9087de2841bf2d59a0823c3b99767af31d79cf4bf9286fd2c18ad1cb1b32cb3dd50404c21eea5e369b1b3d62730f03f3541f20de51ec9acc7f'
