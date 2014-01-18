6031621d6f2900a622a660ec3f4103ce0efdc18e
Sami Koskinen <sami.koskinen@helsinki.fi>
2014-01-18 14:07:50 +0200

    implement ratings

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Ratebeer::Application.routes.draw do
   resources :beers
   resources :breweries
 
+  get 'ratings', to: 'ratings#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
