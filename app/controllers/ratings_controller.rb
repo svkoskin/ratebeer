addb1e430d588d70e5780545bbc503de69ba21c3
Sami Koskinen <sami.koskinen@helsinki.fi>
2014-01-18 15:28:49 +0200

    Creating ratings (2.7 etc.)

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,4 +2,18 @@ class RatingsController < ApplicationController
   def index
     @ratings = Rating.all
   end
+
+  def new
+    @rating = Rating.new
+    @beers = Beer.all
+  end
+
+  def create
+    Rating.create rating_params
+    redirect_to ratings_path
+  end
+
+  def rating_params
+    params.require(:rating).permit(:score, :beer_id)
+  end
 end
