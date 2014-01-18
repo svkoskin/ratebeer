8103c6b49632fa14bd984a82d9d1645e6cdf69a8
Sami Koskinen <sami.koskinen@helsinki.fi>
2014-01-18 15:42:17 +0200

    Redirect to beers lsiting instead of the created beer

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -45,7 +45,7 @@ class BeersController < ApplicationController
 
     respond_to do |format|
       if @beer.save
-        format.html { redirect_to @beer, notice: 'Beer was successfully created.' }
+        format.html { redirect_to beers_path, notice: 'Beer was successfully created.' }
         format.json { render json: @beer, status: :created, location: @beer }
       else
         format.html { render action: "new" }
