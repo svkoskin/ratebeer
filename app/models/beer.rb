47d5d73c3c7701b0976ae4744428422175e248ab
Sami Koskinen <sami.koskinen@helsinki.fi>
2014-01-18 14:54:11 +0200

    Implement average ratings (2.4)

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,12 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    if ratings.empty?
+      -1
+    else
+      ratings.all.inject(0) { |s, r| s += r.score } / ratings.count
+    end
+  end
 end
