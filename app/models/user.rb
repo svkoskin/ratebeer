08a9d90c7801a784d6e8689aab634bf643aff80c
Sami Koskinen <sami.koskinen@helsinki.fi>
2014-01-26 19:29:17 +0200

    Destroy beer club memberships when a user is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
   }
 
   has_many :ratings, dependent: :destroy
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :beer_clubs, through: :memberships
   has_many :beers, through: :ratings
 end
