ab4efdb7ba6f58cc2cdb386fbe7370689b01af9c
Sami Koskinen <sami.koskinen@helsinki.fi>
2014-01-26 17:32:23 +0200

    Password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,17 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :username, uniqueness: true, length: { minimum: 3, maximum: 15 }
+  validates :username, uniqueness: true, length: { 
+    minimum: 3, 
+    maximum: 15 
+  }
+  validates :password, format: {
+    with: /\A(.*\d+.*[A-Z]+.*)|(.*[A-Z]+.*\d+.*)\z/,
+    message: "should include at least one capital letter and at least one digit"
+  }
+  validates :password, length: {
+    minimum: 4
+  }
 
   has_many :ratings
   has_many :beers, through: :ratings
