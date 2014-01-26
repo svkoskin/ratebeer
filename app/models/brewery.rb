4c3dbf6960b30c24f8e4cb580b9664506c0c3a37
Sami Koskinen <sami.koskinen@helsinki.fi>
2014-01-26 16:45:59 +0200

    Reload-compatible year validation for breweries (3.6)

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,14 +1,21 @@
 class Brewery < ActiveRecord::Base
   include AverageRating
 
+  def year_cannot_be_in_the_future
+    if year.present? && year > Time.now.year
+      errors.add(:year, "can't be in the future")
+    end
+  end
+
   has_many :beers, :dependent => :destroy
   has_many :ratings, :through => :beers
 
   validates :name, presence: true
+
+  validate :year_cannot_be_in_the_future
   
   validates :year, numericality: {
     greater_than_or_equal_to: 1042,
-    less_than_or_equal_to: 2014,
     only_integer: true
   }
 
