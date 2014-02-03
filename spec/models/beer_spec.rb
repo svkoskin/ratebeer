b949fb04dcf35e25cd8be50c29338af4eef0a740
Sami Koskinen <sami.koskinen@helsinki.fi>
2014-02-03 18:26:51 +0200

    rspecs for Beer (4.2)

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Beer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is saved with name and style" do
+    beer = Beer.create name:"Kilju", style:"Weizen"
+
+    expect(beer.valid?).to be(true)
+    expect(Beer.count).to eq(1)
+  end
+
+  it "is not saved without a name" do
+    beer = Beer.create name:"", style:"Weizen"
+
+    expect(beer.valid?).to be(false)
+    expect(Beer.count).to eq(0)
+  end
+
+  it "is not saved without a style" do
+    beer = beer.create name:"Kilju", style:""
+
+    expect(beer.valid?).to be(false)
+    expect(Beer.count).to eq(0)
+  end
 end
