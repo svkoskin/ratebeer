d13d01be83a3230737566f3c2d29763ae1fd6efb
Sami Koskinen <sami.koskinen@helsinki.fi>
2014-02-03 20:41:58 +0200

    lol wot

---
+++ spec/features/beer_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "Beer" do
+  let!(:brewery) { FactoryGirl.create :brewery }
+
+  it "can be added with a valid name" do
+    visit new_beer_path
+    fill_in("beer_name", with:"Koff 3:me")
+
+    expect{
+      click_button('Create Beer')
+    }.to change{Beer.count}.by(1)
+  end
+  
+  it "adding beer without a name will fail" do
+    visit new_beer_path
+    fill_in("beer_name", with:"")
+
+    expect{
+      click_button('Create Beer')    
+    }.to change{Beer.count}.by(0)
+
+    puts current_path
+
+    expect(current_path).to eq(beers_path)
+    expect(page).to have_content "Name can't be blank"
+  end
+end
