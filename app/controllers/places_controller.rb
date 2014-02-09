f56cf0ca548c26fb37f9c6354219c1ead30c5ce0
Sami Koskinen <sami.koskinen@helsinki.fi>
2014-02-09 12:55:56 +0200

    Make beer place search a class

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,18 +3,11 @@ class PlacesController < ApplicationController
   end
 
   def search
-    beermapping_api_key = '656e871044a324d7d6fea7efc11f3f1a'
-    places_url = "http://beermapping.com/webservice/loccity/#{beermapping_api_key}/"
-    places_response = HTTParty.get "#{places_url}#{ERB::Util.url_encode(params[:city])}"
-    places_from_bmp = places_response.parsed_response["bmp_locations"]["location"]
+    @places = BeermappingApi.places_in(params[:city])
 
-    if places_from_bmp.is_a?(Hash) and places_from_bmp['id'].nil?
+    if @places.empty?
       redirect_to places_path, :notice => "No places in #{params[:city]}"
     else
-      places_from_bmp = [places_from_bmp] if places_from_bmp.is_a?(Hash)
-      @places = places_from_bmp.inject([]) do |set, location|
-        set << Place.new(location)
-      end
       render :index
     end
   end
---
+++ lib/beermapping_api.rb
@@ -0,0 +1,18 @@
+class BeermappingApi
+  def self.places_in(city)
+    places_url = "http://beermapping.com/webservice/loccity/#{api_key}/"
+    places_response = HTTParty.get "#{places_url}#{ERB::Util.url_encode(city)}"
+    places_from_bmp = places_response.parsed_response["bmp_locations"]["location"]
+
+    return [] if places_from_bmp.is_a?(Hash) and places_from_bmp['id'].nil?
+
+    places_from_bmp = [places_from_bmp] if places_from_bmp.is_a?(Hash)
+    places_from_bmp.inject([]) do |set, location|
+      set << Place.new(location)
+    end
+  end
+
+  def self.api_key
+    '656e871044a324d7d6fea7efc11f3f1a'
+  end
+end
