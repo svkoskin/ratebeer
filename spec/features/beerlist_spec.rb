a20a8a45fec7e7627161bb75d8d052110ddccf23
Sami Koskinen <sami.koskinen@helsinki.fi>
2014-02-15 15:21:23 +0200

    Tests for beer ordering (6.4)

--- spec/features/beerlist_spec.rb
+++ spec/features/beerlist_spec.rb
@@ -37,4 +37,22 @@ describe "Beerlist page" do
     find('table').find('tr:nth-child(3)').should have_content('Lechte Weisse')
     find('table').find('tr:nth-child(4)').should have_content('Nikolai')
   end
+
+  it "beers can be ordered by their style", js:true do
+    visit beerlist_path
+    click_link 'Style'
+
+    find('table').find('tr:nth-child(2)').should have_content('Lager')
+    find('table').find('tr:nth-child(3)').should have_content('Rauchbier')
+    find('table').find('tr:nth-child(4)').should have_content('Weizen')
+  end
+
+  it "beers can be ordered by names of their breweries", js:true do
+    visit beerlist_path
+    click_link 'Brewery'
+
+    find('table').find('tr:nth-child(2)').should have_content('Ayinger')
+    find('table').find('tr:nth-child(3)').should have_content('Koff')
+    find('table').find('tr:nth-child(4)').should have_content('Schlenkerla')
+  end
 end
