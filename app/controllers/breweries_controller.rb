d46fbcb4b5404d9c4ba2ebc5dfe0173ec69a4f66
Sami Koskinen <sami.koskinen@helsinki.fi>
2014-02-15 18:37:22 +0200

    Toggle activity of a brewery (6.11)

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -47,6 +47,14 @@ class BreweriesController < ApplicationController
     @brewery = Brewery.find(params[:id])
   end
 
+  def toggle_activity
+    brewery = Brewery.find(params[:id])
+    brewery.update_attribute :active, (not brewery.active)
+
+    new_status = brewery.active? ? "active" : "retired"
+    redirect_to :back, notice:"brewery activity status changed to #{new_status}"
+  end
+
   # POST /breweries
   # POST /breweries.json
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Ratebeer::Application.routes.draw do
 
   resources :beers
   resources :breweries
+
+  resources :breweries do
+    post 'toggle_activity', on: :member
+  end
+
   resources :ratings, :only => [:index, :new, :create, :destroy]
 
   get 'beerlist', to:'beers#list'
